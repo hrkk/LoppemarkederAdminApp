7f420ccf9be4e18a902386abb62e4d5e42dffbf7
kasper odgaard <odgaard-kasper76@get2net.dk>
2014-02-03 10:42:33 +0100

    after update

--- src/groovy/dk/roninit/dk/MarkedItemView.groovy
+++ src/groovy/dk/roninit/dk/MarkedItemView.groovy
@@ -25,27 +25,6 @@ class MarkedItemView {
         return latitude
     }
 
-    void setLatitude(BigDecimal latitude) {
-        if(latitude==null) {
-            latitude = new BigDecimal(0)
-        }
-        latitude.setScale(9)
-        this.latitude = latitude
-    }
-
-    BigDecimal getLongitude() {
-        return longitude
-    }
-
-    void setLongitude(BigDecimal longitude) {
-        if(longitude==null) {
-            longitude = new BigDecimal(0)
-        }
-        longitude.setScale(9)
-        this.longitude = longitude
-    }
-
-
     public String getFromDateAsString() {
         getDateAsString(fromDate)
     }
@@ -54,58 +33,63 @@ class MarkedItemView {
         getDateAsString(toDate)
     }
 
-
-    @Override
-    public java.lang.String toString() {
-        return "MarkedItemView{" +
-                "name='" + name + '\'' +
-                ", additionalOpenTimePeriod='" + additionalOpenTimePeriod + '\'' +
-                ", entreInfo='" + entreInfo + '\'' +
-                ", markedRules='" + markedRules + '\'' +
-                ", markedInformation='" + markedInformation + '\'' +
-                ", address='" + address + '\'' +
-                ", fromDate=" + fromDate +
-                ", toDate=" + toDate +
-                ", latitude=" + latitude +
-                ", longitude=" + longitude +
-                ", organizerName='" + organizerName + '\'' +
-                ", organizerEmail='" + organizerEmail + '\'' +
-                ", organizerPhone='" + organizerPhone + '\'' +
-                '}';
-    }
-
-    String toHtml() {
-        StringBuilder sb = new StringBuilder(256)
-
-        sb.append("<b>").append("Markeds Arrangør").append("</b>").append("</br>")
-        sb.append("Navn :").append(organizerName).append("</br>")
-        sb.append("E-mail :").append(organizerEmail).append("</br>")
-        sb.append("Telefon nr. :").append(organizerPhone).append("</br>")
-        sb.append("<b>").append("Markeds data").append("</b>").append("</br>")
-        sb.append("Markedsnavn :").append(name).append("</br>")
-        sb.append("Adresse :").append(address).append("</br>")
-        sb.append("Fra dato :").append(getDateAsString(fromDate)).append("</br>")
-        sb.append("Til dato :").append(getDateAsString(toDate)).append("</br>")
-        sb.append("Evt. åbent tidsrum :").append(additionalOpenTimePeriod).append("</br>")
-        sb.append("Entre pris :").append(entreInfo).append("</br>")
-        sb.append("Regler :").append(markedRules).append("</br>")
-        sb.append("Markeds information :").append(markedInformation).append("</br>")
-        return sb.toString()
-    }
-
-
-
     String getDateAsString(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy")
         String formattedDate = sdf.format(date)
         return formattedDate
     }
 
-    String danishCharFix(String w) {
-        return w.replaceAll("::OE::", "Ø")
+    def getName() {
+        danishCharFix(name)
+    }
+
+    def getAdditionalOpenTimePeriod() {
+        danishCharFix(additionalOpenTimePeriod)
     }
 
     def getAddress() {
-        return danishCharFix(address)
+        danishCharFix(address)
+    }
+
+    def getEntreInfo() {
+        danishCharFix(entreInfo)
+    }
+
+    def getMarkedRules() {
+        danishCharFix(markedRules)
+    }
+
+    def getMarkedInformation() {
+        danishCharFix(markedInformation)
+    }
+
+    def getOrganizerName() {
+        danishCharFix(organizerName)
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "MarkedItemView{" +
+                "name='" + getName() + '\'' +
+                ", additionalOpenTimePeriod='" + getAdditionalOpenTimePeriod() + '\'' +
+                ", entreInfo='" + getEntreInfo() + '\'' +
+                ", markedRules='" + getMarkedRules() + '\'' +
+                ", markedInformation='" + getMarkedInformation() + '\'' +
+                ", address='" + getAddress() + '\'' +
+                ", fromDate=" + fromDate +
+                ", toDate=" + toDate +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", organizerName='" + getOrganizerName() + '\'' +
+                ", organizerEmail='" + organizerEmail + '\'' +
+                ", organizerPhone='" + organizerPhone + '\'' +
+                '}';
+    }
+
+    private String danishCharFix(String w) {
+        if(w!= null) {
+            return w.replaceAll("::OE::", "Ø").replaceAll("::AE::", "Æ").replaceAll("::AA::", "Å")
+        }
+        return w
     }
 }
